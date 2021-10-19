a15fd3ae1241c304729a4c48a25c910c6fab1bdd
Andres <andres.z.jimenez15@gmail.com>
2021-10-19 00:10:48 -0500

    Comenzando la integracion de Mercado Pago

--- carrito.php
+++ carrito.php
@@ -27,42 +27,46 @@ incluirTemplate('header');
         <div class="contenido__carrito">
             <h3>Contenido</h3>
             <!-- Contenido -->
+
+            
             <div class="caja__carrito">
                 <p>Productos</p>
+                <div class="carrito__elementos">
+                    <div class="carrito__cantidad">
+                        <span class="thead__carrito">Cantidad</span>
 
-                <table>
-                    <thead class="thead__carrito">
-                        <th>Cantidad</th>
-                        <th>Imagen</th>
-                        <th>Descripcion</th>
-                        <th>Precio</th>
-                        <th>Acciones</th>
-                    </thead>
-                    <tbody>
+                        <div class="carrito__contadores">
+                            <div class="carrito__contador">
+                                <!-- Estan colocados de esta manera para que se actualizen segun la cantidad idsponible -->
+                                <select name="carrito[cantidad]" id="cantidad">
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3" selected>3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                    <option value="6">6</option>
+                                </select>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div >
+                        <img class="carrito__imagen" src="imagenes/4d68706d39f7437e7cfd17667c23bd06.jpeg" alt="imagen de la tabla">
+                    </div>
+                    <div class="carrito__datos">
+                        <span class="thead__carrito">Informacion del Producto</span>
 
-                        <td>
-                            <select name="carrito[cantidad]" id="cantidad">
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3" selected>3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                                <option value="6">6</option>
-                            </select>
-                        </td>
-                        <td>
-                            <img class="imagen-tabla" src="imagenes/4d68706d39f7437e7cfd17667c23bd06.jpeg" alt="imagen de la tabla">
-                        </td>
-                        <td>Gorra Adidas Black</td>
-                        <td>$470</td>
-                        <td>
-                        <button>
-                        Actualizar Compra
-                        </button>
-                        </td>
+                        <div class="arreglo">
+                            <div class="datos__info">
+                                <p>Descripcion: <span>Gorra Adidas Blank</span></p>
+                                <p>Precio Unitario: <span>$ 540</span></p>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
 
-                    </tbody>
-                </table>
+                
             </div>
         
         </div>
